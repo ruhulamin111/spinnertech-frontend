01c94008bd3594048c4a6d730f459251211a3e84
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-12-25 16:16:06 +0600

    title set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='max-w-7xl md: w-10/12 mx-auto h-screen'>
-            <h1>Spinner Tech</h1>
+        <div className='h-screen max-w-7xl md:w-10/12 mx-auto'>
+            {/* title part  */}
+            <section>
+                <h1
+                    className='text-2xl font-bold text-center py-5'
+                >
+                    Spinner Tech
+                </h1>
+            </section>
+            {/* form part  */}
+            <section>
+
+            </section>
         </div>
     )
 }
