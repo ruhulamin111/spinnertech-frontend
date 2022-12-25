8a8b4e2243c8ca9a955074dc98d865cb1419f146
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-12-25 15:58:08 +0600

    color and background set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <div>
-      <h1>Spinner Tech</h1>
+      <Home />
+
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='max-w-7xl md: w-10/12 mx-auto h-screen'>
+            <h1>Spinner Tech</h1>
+        </div>
+    )
+}
+
+export default Home
